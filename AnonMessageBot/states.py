928fd0f0d733a2b9a65ce939e78ee553f5f6fa20
avlExe <avel.strelnikov@outlook.com>
2025-06-01 22:23:14 +0700

    Delete AnonMessageBot directory

--- AnonMessageBot/states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class MessageStates(StatesGroup):
-    waiting_for_username = State()
-    waiting_for_title = State()
-    waiting_for_message = State()
-    waiting_for_confirmation = State()
